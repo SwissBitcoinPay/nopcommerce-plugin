95ce3b9c9118243c91c005910e288559b4e13306
Nisaba <34550856+Nisaba@users.noreply.github.com>
2023-09-20 19:08:25 +0200

    revision 1

---
+++ Components/PaymentSwissBitcoinPayViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Payments.SwissBitcoinPay.Components
+{
+    public class PaymentSwissBitcoinPayViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Plugins/Payments.SwissBitcoinPay/Views/PaymentInfo.cshtml");
+        }
+    }
+}
---
+++ SwissBitcoinPaySettings.cs
@@ -0,0 +1,32 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Payments.SwissBitcoinPay
+{
+    /// <summary>
+    /// Represents settings of BtcPay payment plugin
+    /// </summary>
+    public class SwissBitcoinPaySettings : ISettings
+    {
+        /// <summary>
+        /// The url of your the Swiss Bitcoin Pay API
+        /// </summary>
+        public string ApiUrl { get; set; }
+
+        /// <summary>
+        /// Your Swiss Bitcoin Pay API Key given in your Swiss Bitcoin Pay dashbord
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// The Api Secret value set in your Swiss Bitcoin Pay dashbord
+        /// </summary>
+        public string ApiSecret { get; set; }
+
+        public bool AcceptOnChain { get; set; }
+
+        public decimal AdditionalFee { get; set; }
+
+        public bool AdditionalFeePercentage { get; set; }
+
+    }
+}
